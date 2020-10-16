7530820eea0dbbbe8b502d52f3f941dd38ac57bd
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-16 14:43:55 -0700

    this adds more ot HomeController. utilizes viewbag

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      List<Engineer> model = _db.Engineers.OrderBy(x => x.Name).ToList();
+      return View(model);
     }
   }
 }
--- Models/FactoryContext.cs
+++ Models/FactoryContext.cs
@@ -5,8 +5,8 @@ namespace Factory.Models
 {
   public class FactoryContext : DbContext
   {
-    public DbSet<Engineer> Engineers { get; set; }
-    public DbSet<Machine> Machines { get; set; }
+    public virtual DbSet<Engineer> Engineers { get; set; }
+    public virtual DbSet<Machine> Machines { get; set; }
     public DbSet<License> Licenses { get; set; }
 
     public FactoryContext(DbContextOptions options) : base(options) { }
